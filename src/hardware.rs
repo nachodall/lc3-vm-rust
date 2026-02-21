49f77d93d8d826144427b01e522faba07afb46cb
nachodall <ignaciodallacqua@gmail.com>
2026-02-21 00:23:15 -0300

    feat: implement read_memory and write_memory
    Closes #2

--- src/hardware.rs
+++ src/hardware.rs
@@ -1,33 +1,33 @@
 use libc::{
-    ECHO, FD_SET, FD_ZERO, ICANON, STDIN_FILENO, TCSANOW, VMIN, VTIME, tcgetattr, tcsetattr,
-    termios,
+    ECHO, FD_SET, FD_ZERO, ICANON, STDIN_FILENO, TCSANOW, VMIN, VTIME, fd_set, select, tcgetattr,
+    tcsetattr, termios, timeval,
 };
-use libc::{fd_set, select, timeval};
 
+#[allow(dead_code)]
 static mut ORIGINAL_TERMINAL_SETTINGS: Option<termios> = None;
 
-/// Configures the terminal for "raw" input by disabling line buffering and echoing.
 pub fn disable_input_buffering() {
     unsafe {
         let mut tty: termios = std::mem::zeroed();
-        tcgetattr(STDIN_FILENO, &mut tty);
-        ORIGINAL_TERMINAL_SETTINGS = Some(tty);
-        let mut raw = tty;
-        // ICANON: Disables canonical mode (waits for Enter).
-        // ECHO: Disables automatic printing of typed characters.
-        raw.c_lflag &= !(ICANON | ECHO);
-        // Ensure read returns immediately after 1 character.
-        raw.c_cc[VMIN] = 1;
-        raw.c_cc[VTIME] = 0;
+        if tcgetattr(STDIN_FILENO, &mut tty) == 0 {
+            ORIGINAL_TERMINAL_SETTINGS = Some(tty);
+            let mut raw = tty;
+            // ICANON: Disables canonical mode (waits for Enter).
+            // ECHO: Disables automatic printing of typed characters.
+            raw.c_lflag &= !(ICANON | ECHO);
+            // Ensure read returns immediately after 1 character.
+            raw.c_cc[VMIN] = 1;
+            raw.c_cc[VTIME] = 0;
 
-        tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+            let _ = tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+        }
     }
 }
 
 pub fn restore_input_buffering() {
     unsafe {
         if let Some(tty) = ORIGINAL_TERMINAL_SETTINGS {
-            tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+            let _ = tcsetattr(STDIN_FILENO, TCSANOW, &tty);
         }
     }
 }
@@ -36,12 +36,12 @@ pub fn check_key() -> bool {
     unsafe {
         let mut readfds: fd_set = std::mem::zeroed();
         FD_ZERO(&mut readfds);
-        // Set the bit for STDIN (the keyboard)
         FD_SET(STDIN_FILENO, &mut readfds);
         let mut timeout = timeval {
             tv_sec: 0,
             tv_usec: 0,
         };
+
         select(
             STDIN_FILENO + 1,
             &mut readfds,
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod hardware;
 mod vm;
+use hardware::{disable_input_buffering, restore_input_buffering};
 use vm::*;
 
 fn main() {
+    disable_input_buffering();
     let _lc3 = Vm::new();
+    restore_input_buffering();
 }
